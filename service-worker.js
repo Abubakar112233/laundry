055e17721cc82ba8c8ae61b680dc259e082e6c2e
YannickSpoerl <33640025+YannickSpoerl@users.noreply.github.com>
2020-06-18 12:17:18 +0000

    Deploying to gh-pages from  @ 8da4c825625ad01b29de015a046c4e49c78fd39b 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e8d84abf833fe8f278d88525c9088d5e.js"
+  "/precache-manifest.3cdec1ad31959024be9429e26f9d8ba0.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "laundry"});
